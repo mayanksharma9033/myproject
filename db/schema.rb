116aa7fe6264c535e1edbad34673d1fa27cb8e27
mayank sharma <mayanksharma9033@gmail.com>
2017-05-13 22:12:47 +0530

    create users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513145918) do
+ActiveRecord::Schema.define(version: 20170513152337) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170513145918) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
