e909c0f6c28942c7e2bdefa5170286feb977a8c4
mayank sharma <mayanksharma9033@gmail.com>
2017-05-14 15:44:18 +0530

    create new and edit forms and partials

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+
+def index
+ @posts=Post.all
+end
+
+def show
+@post=Post.find(params[:id])
+end
+
+def new
+@post=Post.new
+
+end
+def create
+@post=Post.new(post_params)
+@post.user=User.find(2)
+
+if @post.save
+	flash[:success]="Your profile was created successfully!"
+
+redirect_to posts_path
+else
+	render :new
+end
+
+end
+
+def edit
+ @post=Post.find(params[:id])
+end
+
+def update
+	@post=Post.find(params[:id])
+
+	if @post.update(post_params)
+		flash[:success]="Your Post was updated successfully!"
+		redirect_to post_path(@post)
+
+	else
+		render :edit
+	end
+
+end
+
+
+private
+
+def post_params
+ params.require(:post).permit(:name,:summary,:description)
+end
+
+end
